fe17b564e2469803431b0c9bbaf1230aa5169e4c
walid-faruok <wilamedo@gmail.com>
2022-01-03 15:59:16 -0800

    created Temblate-Educa

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// started iconse
+let iconse = document.querySelector(".iconse");
+let colora = document.querySelector(".colora");
+let ex = document.querySelector(".ex");
+let img = document.querySelector(".imge");
+
+let arrayimg = [
+    "img/pad1.jpg",
+    "img/pad2.jpg",
+    "img/pad3.jpg",
+    "img/pad6.jpg",
+    "img/pad7.jpg",
+    "img/pad5.jpg"
+];
+
+setInterval(()=>{
+let random = Math.floor(Math.random() * arrayimg.length);
+img.src = arrayimg[random];
+},2000);
+
+iconse.addEventListener("click",function(){
+ colora.classList.toggle("add");
+});
+
+ex.onclick = function(){
+    this.parentElement.classList.remove("add");
+};
+// end iconse
+// start cont
+let ib1 = document.querySelectorAll(".ib1 .cont");
+let dfals = false;
+
+window.onscroll = function(){
+    if(window.scrollY >= 200){
+        if(!dfals){
+            ib1.forEach((col) => addcint(col));
+        }
+        dfals = true;
+    }
+};
+
+function addcint(el){
+let cont = el.dataset.cont;
+let cler = setInterval(()=>{
+  el.textContent++;
+  if(el.textContent == cont){
+     clearInterval(cler);
+  }
+},2000 /cont );
+};
+// end cont
+// start up
+let up = document.querySelector(".up");
+
+window.onscroll = function(){
+    if(window.scrollY >= 1000){
+      up.classList.add("add");
+    }else{
+        up.classList.remove("add");
+    }
+};
+up.addEventListener("click",function(){
+  window.scrollTo({
+     top:0,
+     behavior:"smooth" 
+  });
+});
